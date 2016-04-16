477daafead0b95efa9ec72ae5c45e6dea20be5d1
CobbYbb <gudghks0825@naver.com>
2016-04-16 18:35:40 +0900

    0416 1. 크로스 도메인 이슈 관련 필터 적용 2. view/common/ajaxTest.html 추가 _ 유형환

---
+++ SpringJobs/src/main/java/com/springjobs/common/filter/SimpleCORSFilter.java
@@ -0,0 +1,26 @@
+package com.springjobs.common.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class SimpleCORSFilter implements Filter {
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse response = (HttpServletResponse) res;
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type, Accept");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        chain.doFilter(req, res);
+	}
+	public void init(FilterConfig filterConfig) throws ServletException {}
+	public void destroy() {}
+}
