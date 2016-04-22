074d0d81082936e18c015867f46ad88844f68e15
BaekSeungHyun <bsh2369@gmail.com>
2016-04-22 18:03:49 +0900

    기업프로젝트등록도메인수_백승

--- SpringJobs/src/main/java/com/springjobs/domain/Cpjts.java
+++ SpringJobs/src/main/java/com/springjobs/domain/Cpjts.java
@@ -7,6 +7,7 @@ public class Cpjts {
 	 private int cpjno;
 	 private String cpjnm;
 	 private String cpjint;
+	 private String cpjsk;
 	 private String cpjchk;
 	 private String cpjyr;
 	 private int cpjnpl;
@@ -19,18 +20,20 @@ public class Cpjts {
 	 private String cpjrg;
 	 private String cpjwt;
 	 private String cpjar;
-	 
-	 public Cpjts() {
+	 private int cno;
+
+	public Cpjts() {
 		// TODO Auto-generated constructor stub
 	}
-	 
-	 
-	public Cpjts(int cpjno, String cpjnm, String cpjint, String cpjchk, String cpjyr, int cpjnpl, int cpjmnp,
-			int cpjmap, int cpjhtn, Date cpjrd, Date cpjed, Date cpjsd, String cpjrg, String cpjwt, String cpjar) {
+	
+	public Cpjts(int cpjno, String cpjnm, String cpjint, String cpjsk, String cpjchk, String cpjyr, int cpjnpl,
+			int cpjmnp, int cpjmap, int cpjhtn, Date cpjrd, Date cpjed, Date cpjsd, String cpjrg, String cpjwt,
+			String cpjar, int cno) {
 		super();
 		this.cpjno = cpjno;
 		this.cpjnm = cpjnm;
 		this.cpjint = cpjint;
+		this.cpjsk = cpjsk;
 		this.cpjchk = cpjchk;
 		this.cpjyr = cpjyr;
 		this.cpjnpl = cpjnpl;
@@ -43,108 +46,151 @@ public class Cpjts {
 		this.cpjrg = cpjrg;
 		this.cpjwt = cpjwt;
 		this.cpjar = cpjar;
+		this.cno = cno;
 	}
-
 
 	public int getCpjno() {
 		return cpjno;
 	}
+
 	public void setCpjno(int cpjno) {
 		this.cpjno = cpjno;
 	}
+
 	public String getCpjnm() {
 		return cpjnm;
 	}
+
 	public void setCpjnm(String cpjnm) {
 		this.cpjnm = cpjnm;
 	}
+
 	public String getCpjint() {
 		return cpjint;
 	}
+
 	public void setCpjint(String cpjint) {
 		this.cpjint = cpjint;
 	}
+
+	public String getCpjsk() {
+		return cpjsk;
+	}
+
+	public void setCpjsk(String cpjsk) {
+		this.cpjsk = cpjsk;
+	}
+
 	public String getCpjchk() {
 		return cpjchk;
 	}
+
 	public void setCpjchk(String cpjchk) {
 		this.cpjchk = cpjchk;
 	}
+
 	public String getCpjyr() {
 		return cpjyr;
 	}
+
 	public void setCpjyr(String cpjyr) {
 		this.cpjyr = cpjyr;
 	}
+
 	public int getCpjnpl() {
 		return cpjnpl;
 	}
+
 	public void setCpjnpl(int cpjnpl) {
 		this.cpjnpl = cpjnpl;
 	}
+
 	public int getCpjmnp() {
 		return cpjmnp;
 	}
+
 	public void setCpjmnp(int cpjmnp) {
 		this.cpjmnp = cpjmnp;
 	}
+
 	public int getCpjmap() {
 		return cpjmap;
 	}
+
 	public void setCpjmap(int cpjmap) {
 		this.cpjmap = cpjmap;
 	}
+
 	public int getCpjhtn() {
 		return cpjhtn;
 	}
+
 	public void setCpjhtn(int cpjhtn) {
 		this.cpjhtn = cpjhtn;
 	}
+
 	public Date getCpjrd() {
 		return cpjrd;
 	}
+
 	public void setCpjrd(Date cpjrd) {
 		this.cpjrd = cpjrd;
 	}
+
 	public Date getCpjed() {
 		return cpjed;
 	}
+
 	public void setCpjed(Date cpjed) {
 		this.cpjed = cpjed;
 	}
+
 	public Date getCpjsd() {
 		return cpjsd;
 	}
+
 	public void setCpjsd(Date cpjsd) {
 		this.cpjsd = cpjsd;
 	}
+
 	public String getCpjrg() {
 		return cpjrg;
 	}
+
 	public void setCpjrg(String cpjrg) {
 		this.cpjrg = cpjrg;
 	}
+
 	public String getCpjwt() {
 		return cpjwt;
 	}
+
 	public void setCpjwt(String cpjwt) {
 		this.cpjwt = cpjwt;
 	}
+
 	public String getCpjar() {
 		return cpjar;
 	}
+
 	public void setCpjar(String cpjar) {
 		this.cpjar = cpjar;
 	}
+
+	public int getCno() {
+		return cno;
+	}
+
+	public void setCno(int cno) {
+		this.cno = cno;
+	}
+
 	@Override
 	public String toString() {
-		return "Cpjts [cpjno=" + cpjno + ", cpjnm=" + cpjnm + ", cpjint=" + cpjint + ", cpjchk=" + cpjchk + ", cpjyr="
-				+ cpjyr + ", cpjnpl=" + cpjnpl + ", cpjmnp=" + cpjmnp + ", cpjmap=" + cpjmap + ", cpjhtn=" + cpjhtn
-				+ ", cpjrd=" + cpjrd + ", cpjed=" + cpjed + ", cpjsd=" + cpjsd + ", cpjrg=" + cpjrg + ", cpjwt=" + cpjwt
-				+ ", cpjar=" + cpjar + "]";
-	}
-	 
-
-	 
+		return "Cpjts [cpjno=" + cpjno + ", cpjnm=" + cpjnm + ", cpjint=" + cpjint + ", cpjsk=" + cpjsk + ", cpjchk="
+				+ cpjchk + ", cpjyr=" + cpjyr + ", cpjnpl=" + cpjnpl + ", cpjmnp=" + cpjmnp + ", cpjmap=" + cpjmap
+				+ ", cpjhtn=" + cpjhtn + ", cpjrd=" + cpjrd + ", cpjed=" + cpjed + ", cpjsd=" + cpjsd + ", cpjrg="
+				+ cpjrg + ", cpjwt=" + cpjwt + ", cpjar=" + cpjar + ", cno=" + cno + "]";
+	}	 
 	
 }
