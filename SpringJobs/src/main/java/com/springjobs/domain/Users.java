fe86a1b9f41ec787551fd0c904bfa4c8a14bea6b
CobbYbb <gudghks0825@naver.com>
2016-04-22 20:34:51 +0900

    로그인 세션 cno 추가 _ 유형환

--- SpringJobs/src/main/java/com/springjobs/domain/Users.java
+++ SpringJobs/src/main/java/com/springjobs/domain/Users.java
@@ -17,16 +17,14 @@ public class Users {
 	private String uadr;
 	private String uurl;
 	private String ures;
+	private int cno;
 	
-		
 	public Users() {
 		// TODO Auto-generated constructor stub
 	}
 
-	
-
 	public Users(int uno, String unm, String uenm, String uem, String upw, String uco, String usx, String ucnf,
-			Date ubd, String upho, String uadr, String uurl, String ures) {
+			Date ubd, String upho, String uadr, String uurl, String ures, int cno) {
 		super();
 		this.uno = uno;
 		this.unm = unm;
@@ -41,144 +39,129 @@ public class Users {
 		this.uadr = uadr;
 		this.uurl = uurl;
 		this.ures = ures;
+		this.cno = cno;
 	}
-
-
 
 	public int getUno() {
 		return uno;
 	}
 
-
 	public void setUno(int uno) {
 		this.uno = uno;
 	}
-
 
 	public String getUnm() {
 		return unm;
 	}
 
-
 	public void setUnm(String unm) {
 		this.unm = unm;
 	}
-
 
 	public String getUenm() {
 		return uenm;
 	}
 
-
 	public void setUenm(String uenm) {
 		this.uenm = uenm;
 	}
-
 
 	public String getUem() {
 		return uem;
 	}
 
-
 	public void setUem(String uem) {
 		this.uem = uem;
 	}
-
 
 	public String getUpw() {
 		return upw;
 	}
 
-
 	public void setUpw(String upw) {
 		this.upw = upw;
 	}
-
 
 	public String getUco() {
 		return uco;
 	}
 
-
 	public void setUco(String uco) {
 		this.uco = uco;
 	}
-
 
 	public String getUsx() {
 		return usx;
 	}
 
-
 	public void setUsx(String usx) {
 		this.usx = usx;
 	}
-
 
 	public String getUcnf() {
 		return ucnf;
 	}
 
-
 	public void setUcnf(String ucnf) {
 		this.ucnf = ucnf;
 	}
-
 
 	public Date getUbd() {
 		return ubd;
 	}
 
-
 	public void setUbd(Date ubd) {
 		this.ubd = ubd;
 	}
-
 
 	public String getUpho() {
 		return upho;
 	}
 
-
 	public void setUpho(String upho) {
 		this.upho = upho;
 	}
-
 
 	public String getUadr() {
 		return uadr;
 	}
 
-
 	public void setUadr(String uadr) {
 		this.uadr = uadr;
 	}
-
 
 	public String getUurl() {
 		return uurl;
 	}
 
-
 	public void setUurl(String uurl) {
 		this.uurl = uurl;
 	}
-
 
 	public String getUres() {
 		return ures;
 	}
 
-
 	public void setUres(String ures) {
 		this.ures = ures;
+	}
+
+	public int getCno() {
+		return cno;
+	}
+
+	public void setCno(int cno) {
+		this.cno = cno;
 	}
 
 	@Override
 	public String toString() {
 		return "Users [uno=" + uno + ", unm=" + unm + ", uenm=" + uenm + ", uem=" + uem + ", upw=" + upw + ", uco="
 				+ uco + ", usx=" + usx + ", ucnf=" + ucnf + ", ubd=" + ubd + ", upho=" + upho + ", uadr=" + uadr
-				+ ", uurl=" + uurl + ", ures=" + ures + "]";
+				+ ", uurl=" + uurl + ", ures=" + ures + ", cno=" + cno + "]";
 	}
+	
+	
+	
 	
 } // end of class
